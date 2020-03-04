f285b7cae805597516dbb23e3fc5d66f13c17bff
Tim B <tbatty7@gmail.com>
2020-03-04 17:22:57 -0500

    converted message received to Map

--- src/main/java/com/battybuilds/jenkinstografanaconverter/Controller.java
+++ src/main/java/com/battybuilds/jenkinstografanaconverter/Controller.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class Controller {
 
     @PostMapping("/jenkins-build")
-    public ResponseEntity<String> receiveBuild(@RequestBody String body) {
+    public ResponseEntity<String> receiveBuild(@RequestBody Map body) {
         System.out.println("Build-----------------" + body);
+        System.out.println("Deserialization????????? - " + body.get("jobName"));
         return ResponseEntity.ok("got it");
     }
 
