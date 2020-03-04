0f4ad64f15b78ccba0ff4ec09960d2a1d11ecaea
Tim B <tbatty7@gmail.com>
2020-03-04 17:51:01 -0500

    added Service

--- src/main/java/com/battybuilds/jenkinstografanaconverter/Controller.java
+++ src/main/java/com/battybuilds/jenkinstografanaconverter/Controller.java
@@ -10,12 +10,19 @@ import java.util.Map;
 @RestController
 public class Controller {
 
+    private GrafanaService service;
+
+    public Controller(GrafanaService service) {
+        this.service = service;
+    }
+
     @PostMapping("/jenkins-build")
     public ResponseEntity<String> receiveBuild(@RequestBody Map body) {
         System.out.println("Build-----------------" + body);
         System.out.println("Deserialization????????? - " + body.get("jobName"));
         GrafanaTransformer transformer = new GrafanaTransformer();
         GrafanaRequest transformedRequest = transformer.transform(body);
+
         return ResponseEntity.ok("got it");
     }
 
---
+++ src/main/java/com/battybuilds/jenkinstografanaconverter/GrafanaService.java
@@ -0,0 +1,7 @@
+package com.battybuilds.jenkinstografanaconverter;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GrafanaService {
+}
