c0dd151983d755b46c099bc54e4c3d51bac9b8bf
Tim B <tbatty7@gmail.com>
2020-03-04 17:28:11 -0500

    refactored

--- src/main/java/com/battybuilds/jenkinstografanaconverter/GrafanaRequest.java
+++ src/main/java/com/battybuilds/jenkinstografanaconverter/GrafanaRequest.java
@@ -1,12 +1,11 @@
 package com.battybuilds.jenkinstografanaconverter;
 
 public class GrafanaRequest {
+    String what;
+    String[] tags;
+
     public GrafanaRequest(String what, String[] tags) {
         this.what = what;
         this.tags = tags;
     }
-
-    String what;
-
-    String[] tags;
 }
