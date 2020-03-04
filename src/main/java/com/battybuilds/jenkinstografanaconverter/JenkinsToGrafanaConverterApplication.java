347854a8762c48fdd2529974996a88d1ab5478a6
Tim B <tbatty7@gmail.com>
2020-03-04 17:08:15 -0500

    initial commit

---
+++ src/main/java/com/battybuilds/jenkinstografanaconverter/JenkinsToGrafanaConverterApplication.java
@@ -0,0 +1,13 @@
+package com.battybuilds.jenkinstografanaconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsToGrafanaConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsToGrafanaConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/battybuilds/jenkinstografanaconverter/JenkinsToGrafanaConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battybuilds.jenkinstografanaconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsToGrafanaConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
